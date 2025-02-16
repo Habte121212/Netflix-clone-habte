4886fc6b1d052f65016e560affe15fc6c46faaf1
Habtamu <amarehabtamu978@gmail.com>
2025-02-15 16:58:08 -0800

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './assets/pages/home/home';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
-        <h1> hellow world</h1>
+       <Home/>
       </div>
-      
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/assets/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
+
+
+
