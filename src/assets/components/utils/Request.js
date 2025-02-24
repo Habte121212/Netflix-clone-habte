e9fb4ed4562301dddafba440ee40b36bed27b2a4
Habtamu <amarehabtamu978@gmail.com>
2025-02-24 09:40:55 -0800

    axios and request

---
+++ src/assets/components/utils/Request.js
@@ -0,0 +1,17 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const Request = {
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchTVMovie: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  fetchFamily: `/discover/movie?api_key=${API_KEY}&with_genres=10751`,
+  fetchRomance: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchScienceFiction: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  fetchDocumentary: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchThriller: `/discover/movie?api_key=${API_KEY}&with_genres=53`,
+  fetchAction: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  fetchAdventure: `/discover/movie?api_key=${API_KEY}&with_genres=12`,
+};
+
+export default Request; 
